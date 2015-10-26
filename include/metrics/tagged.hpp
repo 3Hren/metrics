c1135f612cf9a756aa95e2f07a9fdca9facbf84b
Evgeny Safronov <division494@gmail.com>
2015-10-26 15:12:28 +0300

    docs(tagged): add more comments

--- include/metrics/tagged.hpp
+++ include/metrics/tagged.hpp
@@ -7,6 +7,7 @@
 
 namespace metrics {
 
+/// The tagged struct represents tagged metric name.
 class tagged_t {
 public:
     typedef std::map<std::string, std::string> container_type;
@@ -15,18 +16,24 @@ private:
     container_type container;
 
 public:
+    /// Creates a tagged struct with a single `name` tag.
     tagged_t(std::string name);
+
+    /// Creates a tagged struct with the given tags, overriding the `name` tag if required.
     tagged_t(std::string name, container_type tags);
 
     virtual
     ~tagged_t();
 
+    /// Returns a const reference to the underlying tag map.
     const container_type&
     tags() const noexcept;
 
+    /// Returns a const reference to the name tag.
     const std::string&
     name() const noexcept;
 
+    /// Returns the tag value that is mapped to a key equivalent to `key`, `boost::none` otherwise.
     boost::optional<std::string>
     tag(const std::string& key) const;
 
