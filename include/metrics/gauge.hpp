5290be40ce392e873e325b8a06aaeddca02b8354
Evgeny Safronov <division494@gmail.com>
2016-12-19 15:53:50 +0300

    docs: dot

--- include/metrics/gauge.hpp
+++ include/metrics/gauge.hpp
@@ -7,7 +7,7 @@ namespace metrics {
 /// A gauge is the simplest metric type. It just returns a value.
 ///
 /// Useful if, for example, your application has a value which is maintained by a third-party
-/// library, you can easily expose it by registering a gauge instance which returns that value
+/// library, you can easily expose it by registering a gauge instance which returns that value.
 template<typename T>
 using gauge = std::function<T()>;
 
